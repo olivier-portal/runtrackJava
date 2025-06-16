ec8a916b2cb379a8b8d3ee1db728a8ac4171734f
MrRobot <olivier.portal@laplateforme.io>
2025-06-16 12:10:52 +0200

    feat: Job00 OK

---
+++ src/Job00.java
@@ -0,0 +1,17 @@
+package src.Job00;
+
+public class Job00 {
+
+    public static void main(String[] args) {
+        char caractere = 'a';
+        String chaine = "Hello world";
+        int i = 12;
+        long  grosNombre = 987654321000L;
+        float virgule = 3.2F;
+        boolean VraiFaux = true;
+
+        int TOTO = (int) 3.817;
+
+        System.out.println(TOTO);
+    }
+}
